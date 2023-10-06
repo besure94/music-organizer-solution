6001fc20457b64b436780ceb92fdfc39c85b5e2e
Brian Scherner <brianscherner@gmail.com>
2023-10-06 14:13:35 -0700

    Initial commit.

---
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MusicOrganizer.Models;
+using System;
+
+namespace MusicOrganizer.Tests
+{
+  [TestClass]
+  public class ArtistTests
+  {
+
+  }
+}
