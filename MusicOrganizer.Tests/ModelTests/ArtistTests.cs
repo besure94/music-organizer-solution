c3b44ea1ac956c893a2ba35a18fbffcd7e0af22a
Brian Scherner <brianscherner@gmail.com>
2023-10-06 15:35:13 -0700

    Add functionality to create Artist constructor + passing test.

--- MusicOrganizer.Tests/ModelTests/ArtistTests.cs
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -2,12 +2,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using MusicOrganizer.Models;
 using System;
+using System.Collections.ObjectModel;
 
 namespace MusicOrganizer.Tests
 {
   [TestClass]
   public class ArtistTests
   {
-
+    [TestMethod]
+    public void ArtistConstructor_CreatesInstanceOfArtist_Artist()
+    {
+      Artist newArtist = new Artist("test artist");
+      Assert.AreEqual(typeof(Artist), newArtist.GetType());
+    }
   }
 }
---
+++ MusicOrganizer/Models/Artist.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Models
+{
+  public class Artist
+  {
+
+
+    public Artist(string name)
+    {
+
+    }
+  }
+}
