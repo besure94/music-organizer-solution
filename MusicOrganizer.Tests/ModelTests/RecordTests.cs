8f207db53c270724ce678f5083e35908a2a3d11d
Brian Scherner <brianscherner@gmail.com>
2023-10-06 14:59:46 -0700

    Add functionality for getting all records and clearing all records + passing test.

--- MusicOrganizer.Tests/ModelTests/RecordTests.cs
+++ MusicOrganizer.Tests/ModelTests/RecordTests.cs
@@ -6,8 +6,14 @@ using System;
 namespace MusicOrganizer.Tests
 {
   [TestClass]
-  public class RecordTests
+  public class RecordTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Record.ClearAll();
+    }
+
     [TestMethod]
     public void RecordConstructor_CreatesInstanceOfRecord_Record()
     {
@@ -39,6 +45,20 @@ namespace MusicOrganizer.Tests
     public void GetAll_ReturnsEmptyList_RecordList()
     {
       List<Record> newList = new List<Record> { };
+      List<Record> result = Record.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsRecords_RecordList()
+    {
+      string title01 = "Definitely Maybe";
+      string title02 = "What's the Story, Morning Glory?";
+      string title03 = "In the Graveyard";
+      Record newRecord01 = new Record(title01);
+      Record newRecord02 = new Record(title02);
+      Record newRecord03 = new Record(title03);
+      List<Record> newList = new List<Record> { newRecord01, newRecord02, newRecord03 };
       List<Record> result = Record.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
--- MusicOrganizer/Models/Record.cs
+++ MusicOrganizer/Models/Record.cs
@@ -10,11 +10,17 @@ namespace MusicOrganizer.Models
     public Record(string title)
     {
       Title = title;
+      _instances.Add(this);
     }
 
     public static List<Record> GetAll()
     {
       return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
